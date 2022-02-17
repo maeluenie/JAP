e7d43d7612be4f626b4b1ebc4d0739e0601ecb6e
tagun9921 <42992928+tagun9921@users.noreply.github.com>
2022-02-17 20:20:11 +0700

    Completed the job application card.
    Awaiting looping throughout the cards to display jobs according to the mock-data representation

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <v-row justify="start" align="start">
     <v-col cols="auto" sm="8" md="6">
-      <v-card class="mx-8 my-4" max-width="700" elevation="4">
-        <v-list-item three-line>
+      <v-card class="ml-4 mt-4" elevation="4">
+        <v-list-item>
           <v-list-item-avatar
             align="center"
             tile
@@ -11,36 +11,107 @@
           ></v-list-item-avatar>
 
           <v-list-item-content>
-            <!-- <div class="text-h6 mb-1 font-weight-bold">OVERLINE</div> -->
-
-            <v-container bg fill-height grid-list-md text-xs-center>
-              <v-layout column wrap align-left>
-                <v-flex>
-                  Hello I am center to vertically using "align-center".
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-list-item-content>
-          <v-list-item-content>
-            <!-- <div class="text-h6 mb-1 font-weight-bold">OVERLINE</div> -->
+            <div class="text-h6 my-1 font-weight-bold">OVERLINE</div>
 
             <v-container bg fill-height align-left>
               <v-layout column align-left>
                 <v-layout row align-left>
-                  <v-flex class="font-weight-bold "> <v-icon class="mr-4"> mdi-domain </v-icon> Ladkrabang HQ </v-flex> 
+                  <v-flex class='my-1'>
+                    Ladkrabang HQ
+                  </v-flex>
                 </v-layout>
                 <v-layout row align-left justify-start>
-                  <v-flex> <v-icon> mdi-domain </v-icon> </v-flex>
-                  <v-flex> B </v-flex>
+                  <v-flex class='my-1'>
+                    <v-icon class="mr-4"> mdi-domain </v-icon> Ladkrabang HQ
+                  </v-flex>
                 </v-layout>
                 <v-layout row align-left>
-                  <v-flex> <v-icon> mdi-domain </v-icon> </v-flex>
-                  <v-flex> B </v-flex>
+                  <v-flex class='my-1'>
+                    <v-icon class="mr-4"> mdi-domain </v-icon> Ladkrabang HQ
+                  </v-flex>
+                </v-layout>
+              </v-layout>
+              <v-layout column align-left>
+                <v-layout row align-left>
+                  <v-flex class='my-1'>
+                   Start Date : April 2022
+                  </v-flex>
+                </v-layout>
+                <v-layout row align-left justify-start>
+                  <v-flex class='my-1'>
+                    Deadline : March 3 , 2022
+                  </v-flex>
+                </v-layout>
+                <v-layout row align-left>
+                  <v-flex class='my-1'>
+                    Approx. Salary ≈ 100,000 THB
+                  </v-flex>
                 </v-layout>
               </v-layout>
             </v-container>
           </v-list-item-content>
         </v-list-item>
+
+        <hr class="my-1" />
+
+        <v-col class="text-right">
+          <v-btn align="end" color="primary" nuxt to="/inspire"> Apply </v-btn>
+        </v-col>
+      </v-card>
+      <div class="row mb-4"></div>
+    </v-col>
+    <v-col cols="auto" sm="8" md="6">
+      <v-card class="ml-4 mt-4" elevation="4">
+        <v-list-item>
+          <v-list-item-avatar
+            align="center"
+            tile
+            size="80"
+            color="grey"
+          ></v-list-item-avatar>
+
+          <v-list-item-content>
+            <div class="text-h6 my-1 font-weight-bold">OVERLINE</div>
+
+            <v-container bg fill-height align-left>
+              <v-layout column align-left>
+                <v-layout row align-left>
+                  <v-flex class='my-1'>
+                    Ladkrabang HQ
+                  </v-flex>
+                </v-layout>
+                <v-layout row align-left justify-start>
+                  <v-flex class='my-1'>
+                    <v-icon class="mr-4"> mdi-domain </v-icon> Ladkrabang HQ
+                  </v-flex>
+                </v-layout>
+                <v-layout row align-left>
+                  <v-flex class='my-1'>
+                    <v-icon class="mr-4"> mdi-domain </v-icon> Ladkrabang HQ
+                  </v-flex>
+                </v-layout>
+              </v-layout>
+              <v-layout column align-left>
+                <v-layout row align-left>
+                  <v-flex class='my-1'>
+                   Start Date : April 2022
+                  </v-flex>
+                </v-layout>
+                <v-layout row align-left justify-start>
+                  <v-flex class='my-1'>
+                    Deadline : March 3 , 2022
+                  </v-flex>
+                </v-layout>
+                <v-layout row align-left>
+                  <v-flex class='my-1'>
+                    Approx. Salary ≈ 100,000 THB
+                  </v-flex>
+                </v-layout>
+              </v-layout>
+            </v-container>
+          </v-list-item-content>
+        </v-list-item>
+
         <hr class="my-1" />
 
         <v-col class="text-right">
@@ -50,6 +121,7 @@
       <div class="row mb-4"></div>
     </v-col>
   </v-row>
+  
 </template>
 
 <script>
