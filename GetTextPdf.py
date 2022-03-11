ab607b4956b62fa0b12099a707986bb7a0e5f2ec
maeluenie <maeluenie@gmail.com>
2022-03-11 22:16:41 +0700

    template for text extraction using pdfminer

---
+++ GetTextPdf.py
@@ -0,0 +1,22 @@
+#Template for text extraction from pdf
+
+from io import StringIO
+
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+output_string = StringIO()
+with open('Sample.pdf', 'rb') as in_file:
+    parser = PDFParser(in_file)
+    doc = PDFDocument(parser)
+    rsrcmgr = PDFResourceManager()
+    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    for page in PDFPage.create_pages(doc):
+        interpreter.process_page(page)
+
+print(output_string.getvalue())
