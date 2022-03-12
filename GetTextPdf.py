6418950acd2b63341d674e90ba8675954561ff64
maeluenie <maeluenie@gmail.com>
2022-03-12 16:54:36 +0700

    get skills function (incomplete)

--- GetTextPdf.py
+++ GetTextPdf.py
@@ -1,4 +1,4 @@
-#Template for text extraction from pdf
+# Template for extraction from resume pdf file
 
 from io import StringIO
 
@@ -12,13 +12,92 @@ from pdfminer.pdfparser import PDFParser
 import os
 import glob
 
+import nltk
+
+#solve nltk can't download needed files
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+nltk.download('stopwords')
+
+SKILLS_DB = [
+    'machine learning',
+    'data science',
+    'python',
+    'word',
+    'excel',
+    'English',
+    'Thai',
+    'React',
+    'Golang',
+    'C',
+    'C++',
+    'C#',
+    'Java',
+    'HTML',
+    'SQL'
+]
+
 path = '/Users/maeluenie/Documents/GitHub/JAP/resume/'
-count =0
+count = 0
+ 
+ 
+def extract_names(txt):
+    person_names = []
+ 
+    for sent in nltk.sent_tokenize(txt):
+        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+            if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
+                person_names.append(
+                    ' '.join(chunk_leave[0] for chunk_leave in chunk.leaves())
+                )
+ 
+    return person_names
+
+def extract_skills(input_text):
+    stop_words = set(nltk.corpus.stopwords.words('english'))
+    word_tokens = nltk.tokenize.word_tokenize(input_text)
+ 
+    # remove the stop words
+    filtered_tokens = [w for w in word_tokens if w not in stop_words]
+ 
+    # remove the punctuation
+    filtered_tokens = [w for w in word_tokens if w.isalpha()]
+ 
+    # generate bigrams and trigrams (such as artificial intelligence)
+    bigrams_trigrams = list(map(' '.join, nltk.everygrams(filtered_tokens, 2, 3)))
+ 
+    # we create a set to keep the results in.
+    found_skills = set()
+ 
+    # we search for each token in our skills database
+    for token in filtered_tokens:
+        if token.lower() in SKILLS_DB:
+            found_skills.add(token)
+ 
+    # we search for each bigram and trigram in our skills database
+    for ngram in bigrams_trigrams:
+        if ngram.lower() in SKILLS_DB:
+            found_skills.add(ngram)
+ 
+    return found_skills
+
+
 for filename in glob.glob(os.path.join(path,'*.pdf')):
     count +=1
     with open(os.path.join(os.getcwd(), filename), 'rb') as f:
+
         output_string = StringIO()
-        #with open(filename, 'rb') as in_file:
+        # with open(filename, 'rb') as in_file:
         parser = PDFParser(f)
         doc = PDFDocument(parser)
         rsrcmgr = PDFResourceManager()
@@ -26,5 +105,9 @@ for filename in glob.glob(os.path.join(path,'*.pdf')):
         interpreter = PDFPageInterpreter(rsrcmgr, device)
         for page in PDFPage.create_pages(doc):
             interpreter.process_page(page)
-        print(output_string.getvalue())
+        
+        content = output_string.getvalue()
+        skills = extract_skills(content)
+
+        print(skills)
         print('-----------------------------------------', count)
