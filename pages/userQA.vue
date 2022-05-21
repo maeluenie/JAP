afa89c54eea771214fab05ac159bdb23994209ae
viewnts <44019863+viewnts@users.noreply.github.com>
2022-05-21 23:56:55 +0700

    Update userQA.vue

--- pages/userQA.vue
+++ pages/userQA.vue
@@ -1,136 +1,151 @@
 <template>
-  <v-app>
-    <!-- addJob2 page will be the second page of job details with navigation after addJob1 -->
-
+  <v-form class="mx-8 mt-2 align-content-center">
+       
     <v-card class="mx-4 my-2 py-2">
-      <v-text-title class="font-weight-bold"
-
-        ><h2 class="mx-8 my-4 text-left">
-          Personalized Questions
-        </h2></v-text-title>
-     
-
-
-      <v-form class="mx-8 mt-2 align-content-center">
-
-        <v-row>
-          <v-col cols="1" sm="1" md="12">
-
-            <v-text class="font"
-              ><h5 class="mx-1 mb-2 text-left">1. What is the first step emphathize users.</h5></v-text
-            >
-
-            <v-textarea
-
-            v-model="jobDescription"
-            :rules="nameRules"
-            label="Job Description"
-            required
-            outlined
-            dense
-
-            ></v-textarea>
-            <v-text class="font"
-              ><h5 class="mx-1 mb-2 text-left">Job Description</h5></v-text
-            >
-
-            <v-textarea
-
-            v-model="jobDescription"
-            :rules="nameRules"
-            label="Job Description"
-            required
-            outlined
-            dense
-
-            ></v-textarea>
-            <v-text class="font"
-              ><h5 class="mx-1 mb-2 text-left">Job Description</h5></v-text
-            >
-
-            <v-textarea
-
-            v-model="jobDescription"
-            :rules="nameRules"
-            label="Job Description"
-            required
-            outlined
-            dense
-
-            ></v-textarea>
-            <v-text class="font"
-              ><h5 class="mx-1 mb-2 text-left">Job Description</h5></v-text
-            >
-
-            <v-textarea
-
-            v-model="jobDescription"
-            :rules="nameRules"
-            label="Job Description"
-            required
-            outlined
-            dense
-
-            ></v-textarea>
-            <v-text class="font"
-              ><h5 class="mx-1 mb-2 text-left">Job Description</h5></v-text
-            >
-
-            <v-textarea
-
-            v-model="jobDescription"
-            :rules="nameRules"
-            label="Job Description"
-            required
-            outlined
-            dense
-
-            ></v-textarea>
-            <v-text class="font"
-              ><h5 class="mx-1 mb-2 text-left">Job Description</h5></v-text
-            >
-
-            <v-textarea
-
-            v-model="jobDescription"
-            :rules="nameRules"
-            label="Job Description"
-            required
-            outlined
-            dense
-
-            ></v-textarea>
-          </v-col>
-
-
-          <v-col class="text-right">
-            <v-btn align="end" color="primary darken-3" nuxt to="/APform4">  
-              Submit
-            </v-btn>
-          </v-col>
-          
-        </v-row>
-      </v-form>
+      <v-text-title>
+        <h2 class="mx-8 my-4 text-left">
+          Specific Questions
+        </h2>
+      </v-text-title>
     </v-card>
-  </v-app>
+
+      
+          <div v-for="(question, i) in all_q" :key="i"> 
+            <v-card  class="mx-4 my-2 py-2">
+              <v-row class="mt-2">
+                <v-col cols="20" sm="20" md="12">
+                  
+                        Question {{i+1}}. {{ question.question}}
+                    
+                      <v-text-field
+                        v-model="question.answer" 
+                        required
+                        outlined
+                        dense
+                        placeholder="Enter Answer"     
+                      />
+              
+                
+                </v-col>
+              </v-row>
+            </v-card>
+          </div>
+          <!-- {{all_q}} -->
+          <v-row>
+          <v-dialog
+            v-model="dialog"
+            width="500"
+          >
+            <template v-slot:activator="{ on, attrs }">
+                <v-col>
+                  <v-btn 
+                  align="end" 
+                  color="primary darken-3"
+                  v-bind="attrs"
+                  v-on="on" 
+                  @click="submit();"
+                  > 
+                    Submit
+                  </v-btn>
+                  
+                </v-col>
+            </template>
+          <v-card>
+            <v-card-title icon ="mdi-check" class="text-h5 grey lighten-2">
+              Submitted!
+            </v-card-title>
+
+            <v-card-text>
+              Thank you for your interest, futhur information will be informed as soon as possible.
+            </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="primary"
+                text
+                @click="dialog = false"
+                nuxt to="/"
+              >
+                OK
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          </v-dialog>
+          </v-row>
+  </v-form>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-  // name: "AddRemove",
-  // data() {
-  //   return {
-  //     Questions: [{ ques: "" }],
-  //   };
-  // },
-  // methods: {
-  //   addField(value, fieldType) {
-  //     fieldType.push({ value: "" });
-  //   },
-  //   removeField(index, fieldType) {
-  //     fieldType.splice(index, 1);
-  //   },
-  // },
-};
+  computed: {
+    all_q2(){
+      return this.all_q.map(value=>{return {
+        question_id: value.q_id,
+        answer: value.answer
+      }})
+    }
+  },
+  data(){
+    return{
+      dialog:false,
+      questions:[],
+      answer: [],
+      all_q: []
+      // user:{
+      //   id:[],
+      //   questions:[]
+      // }
+    }
+  },
+  async created() {
+    const config = {
+      headers:{
+        Authorization : this.$auth.$storage._state["_token.local"],
+        'Accept':'application/json'
+      }
+    }
+    try{
+    const res = await axios.get("https://api.job-application.duckdns.org/displaySelectedQuestions",config);
+    this.questions = res.data.questions;
+    this.qa()
+    console.log(res.data.questions)
+
+    } catch(err) {
+      console.log(err)
+    }
+  },
+  methods:{
+     activate() {
+      setTimeout(() => this.isHidden = false, 500);
+    },
+    async qa(){
+      for(let i of this.questions)
+      this.all_q.push({
+        ...i,
+        answer:""
+      })
+      console.log(this.all_q)
+    },
+    async submit(){
+      const config = {
+        headers:{
+          Authorization : this.$auth.$storage._state["_token.local"],
+        }
+      }
+      await axios.post('https://api.job-application.duckdns.org/postSpecificQuestions',{
+        "list_of_answers":this.all_q.map(value=>{return {
+        q_id: value.q_id,
+        answer: value.answer
+      }})},config)
+    }
+  }
+}
 </script>
-<style></style>
+
+<style>
+
+</style>
